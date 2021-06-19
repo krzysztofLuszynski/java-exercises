37dfa999ae2aa414521fdc6fbaef44eb9eebe0ad
krzysztofLuszynski <krzysztof.luszynski@gmail.com>
2021-06-19 10:31:06 +0200

    Implemented islands with tests.

--- src/main/java/exercise/island/IslandsDetector.java
+++ src/main/java/exercise/island/IslandsDetector.java
@@ -1,12 +1,74 @@
 package exercise.island;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.*;
 
 class IslandsDetector {
-    int getIslandsNumber(final char[][] map) {
+    List<Collection<Position>> getAllIslands(final char[][] map) {
         assertConditions(map);
 
-        return 0;
+        final Map<Position, FieldType> positionToFieldType = getPositionToFieldType(map);
+
+        final List<Position> processedPositions = new ArrayList<>(
+                getAllWaterPositions(positionToFieldType));
+
+        final List<Collection<Position>> allIslands = new ArrayList<>();
+
+        Set<Position> currentIsland = new LinkedHashSet<>();
+
+        while (processedPositions.size() != (map.length * map[0].length)) {
+            Position current = null;
+            if (currentIsland.isEmpty() || processedPositions.containsAll(currentIsland)) {
+                final Optional<Position> firstNotProcessed = findFirstNotProcessedField(positionToFieldType, processedPositions);
+                if (!firstNotProcessed.isPresent()) {
+                    break;
+                }
+
+                current = firstNotProcessed.get();
+                if (!currentIsland.isEmpty() && processedPositions.containsAll(currentIsland)) {
+                    allIslands.add(currentIsland);
+                    currentIsland = new LinkedHashSet<>();
+                }
+
+                currentIsland.add(current);
+            } else {
+                boolean foundNotProcessedIslandField = false;
+                for (Position position : currentIsland) {
+                    if (!processedPositions.contains(position)) {
+                        current = position;
+                        foundNotProcessedIslandField = true;
+                        break;
+                    }
+                }
+
+                if (!foundNotProcessedIslandField) {
+                    allIslands.add(currentIsland);
+                    currentIsland = new LinkedHashSet<>();
+                    continue;
+                }
+            }
+
+            processedPositions.add(current);
+
+            final Position upPosition = new Position(current.getRow() - 1, current.getColumn());
+            checkAndAddToCurrentIsland(upPosition, positionToFieldType, currentIsland);
+            final Position downPosition = new Position(current.getRow() + 1, current.getColumn());
+            checkAndAddToCurrentIsland(downPosition, positionToFieldType, currentIsland);
+            final Position leftPosition = new Position(current.getRow(), current.getColumn() - 1);
+            checkAndAddToCurrentIsland(leftPosition, positionToFieldType, currentIsland);
+            final Position rightPosition = new Position(current.getRow(), current.getColumn() + 1);
+            checkAndAddToCurrentIsland(rightPosition, positionToFieldType, currentIsland);
+        }
+
+        if (!currentIsland.isEmpty()) {
+            allIslands.add(currentIsland);
+        }
+
+        return allIslands;
     }
 
     private void assertConditions(final char[][] map) {
@@ -14,10 +76,130 @@ class IslandsDetector {
             throw new IllegalArgumentException("Map can not be empty or null !");
         }
 
-        for (int rowIndex = 0 ; rowIndex < map.length; rowIndex++) {
+        for (int rowIndex = 0; rowIndex < map.length; rowIndex++) {
             if (ArrayUtils.isEmpty(map[rowIndex])) {
                 throw new IllegalArgumentException("Map row can not be empty or null !");
             }
+
+            for (int columnIndex = 0; columnIndex < map[rowIndex].length; columnIndex++) {
+                final char character = map[rowIndex][columnIndex];
+                if (!isLand(character) && !isWater(character)) {
+                    throw new IllegalArgumentException("Map element[" + rowIndex + "]["
+                            + columnIndex + "] with value: " + character + " is not land or water !");
+                }
+            }
+        }
+    }
+
+    private boolean isLand(final char character) {
+        return character == 'l';
+    }
+
+    private boolean isWater(final char character) {
+        return character == 'w';
+    }
+
+    private Map<Position, FieldType> getPositionToFieldType(final char[][] map) {
+        final Map<Position, FieldType> positionToCharacter = new LinkedHashMap<>();
+
+        for (int rowIndex = 0; rowIndex < map.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < map[rowIndex].length; columnIndex++) {
+                final Position position = new Position(rowIndex, columnIndex);
+
+
+                final char field = map[rowIndex][columnIndex];
+                final FieldType fieldType;
+                if (isWater(field)) {
+                    fieldType = FieldType.WATER;
+                } else {
+                    fieldType = FieldType.LAND;
+                }
+
+                positionToCharacter.put(position, fieldType);
+            }
+        }
+
+        return positionToCharacter;
+    }
+
+    private List<Position> getAllWaterPositions(final Map<Position, FieldType> positionToFieldType) {
+        final List<Position> positions = new ArrayList<>();
+
+        for (Map.Entry<Position, FieldType> mapEntry : positionToFieldType.entrySet()) {
+            if (mapEntry.getValue() == FieldType.WATER) {
+                positions.add(mapEntry.getKey());
+            }
+        }
+
+        return positions;
+    }
+
+    private Optional<Position> findFirstNotProcessedField(final Map<Position, FieldType> positionToFieldType,
+                                                          final List<Position> processed) {
+
+        for (Map.Entry<Position, FieldType> mapEntry : positionToFieldType.entrySet()) {
+            if (!processed.contains(mapEntry.getKey())) {
+                return Optional.of(mapEntry.getKey());
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private void checkAndAddToCurrentIsland(final Position position,
+                                            final Map<Position, FieldType> positionToFieldType,
+                                            final Set<Position> currentIsland) {
+        final Set<Position> allPositions = positionToFieldType.keySet();
+
+        if (allPositions.contains(position) && positionToFieldType.get(position) == FieldType.LAND) {
+            currentIsland.add(position);
+        }
+    }
+
+    private enum FieldType {
+        WATER,
+        LAND
+    }
+
+    static class Position {
+        private final int row;
+        private final int column;
+
+        public Position(final int row, final int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Position position = (Position) o;
+
+            return new EqualsBuilder().append(row, position.row).append(column, position.column).isEquals();
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder(17, 37).append(row).append(column).toHashCode();
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this)
+                    .append("row", row)
+                    .append("column", column)
+                    .toString();
         }
     }
 }
--- src/test/java/exercise/island/IslandsDetectorTest.java
+++ src/test/java/exercise/island/IslandsDetectorTest.java
@@ -2,6 +2,9 @@ package exercise.island;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -10,21 +13,21 @@ class IslandsDetectorTest {
 
     @Test
     void nullMapArray() {
-        assertThatThrownBy(() -> islandsDetector.getIslandsNumber(null))
+        assertThatThrownBy(() -> islandsDetector.getAllIslands(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Map can not be empty or null !");
     }
 
     @Test
     void emptyMapArray() {
-        assertThatThrownBy(() -> islandsDetector.getIslandsNumber(new char[0][0]))
+        assertThatThrownBy(() -> islandsDetector.getAllIslands(new char[0][0]))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Map can not be empty or null !");
     }
 
     @Test
     void nullRowArray() {
-        assertThatThrownBy(() -> islandsDetector.getIslandsNumber(new char[1][]))
+        assertThatThrownBy(() -> islandsDetector.getAllIslands(new char[1][]))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Map row can not be empty or null !");
     }
@@ -33,18 +36,195 @@ class IslandsDetectorTest {
     void emptyRowArray() {
         final char[][] mapWithEmptyRow = new char[1][];
         mapWithEmptyRow[0] = new char[0];
-        assertThatThrownBy(() -> islandsDetector.getIslandsNumber(mapWithEmptyRow))
+        assertThatThrownBy(() -> islandsDetector.getAllIslands(mapWithEmptyRow))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Map row can not be empty or null !");
     }
 
     @Test
+    void invalidCharacterArray() {
+        final char[][] invalidCharacterMap = new char[1][];
+        invalidCharacterMap[0] = new char[]{'x'};
+        assertThatThrownBy(() -> islandsDetector.getAllIslands(invalidCharacterMap))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Map element[0][0] with value: x is not land or water !");
+    }
+
+    @Test
     void onlyWaterMap() {
         final char[][] map = new char[3][];
-        map[0] = new char[]{'w', 'w', 'w'};
-        map[1] = new char[]{'w', 'w', 'w'};
-        map[2] = new char[]{'w', 'w', 'w'};
+        map[0] = new char[]{ 'w', 'w', 'w'};
+        map[1] = new char[]{ 'w', 'w', 'w'};
+        map[2] = new char[]{ 'w', 'w', 'w'};
 
-        assertThat(islandsDetector.getIslandsNumber(map)).isEqualTo(0);
+        assertThat(islandsDetector.getAllIslands(map)).hasSize(0);
+    }
+
+    @Test
+    void onlyLandMap() {
+        final char[][] map = new char[3][];
+        map[0] = new char[]{ 'l', 'l', 'l'};
+        map[1] = new char[]{ 'l', 'l', 'l'};
+        map[2] = new char[]{ 'l', 'l', 'l'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(1);
+        assertThat(allIslands.get(0))
+                .containsOnly(
+                        new IslandsDetector.Position(0, 0),
+                        new IslandsDetector.Position(0, 1),
+                        new IslandsDetector.Position(0, 2),
+                        new IslandsDetector.Position(1, 0),
+                        new IslandsDetector.Position(1, 1),
+                        new IslandsDetector.Position(1, 2),
+                        new IslandsDetector.Position(2, 0),
+                        new IslandsDetector.Position(2, 1),
+                        new IslandsDetector.Position(2, 2)
+                );
+    }
+
+    @Test
+    void singleIslandLeftUpMap() {
+        final char[][] map = new char[3][];
+        map[0] = new char[]{ 'l', 'w', 'w'};
+        map[1] = new char[]{ 'w', 'w', 'w'};
+        map[2] = new char[]{ 'w', 'w', 'w'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(1);
+        assertThat(allIslands.get(0))
+                .containsExactly(new IslandsDetector.Position(0, 0));
+    }
+
+    @Test
+    void singleIslandRightUpMap() {
+        final char[][] map = new char[3][];
+        map[0] = new char[]{ 'w', 'w', 'l'};
+        map[1] = new char[]{ 'w', 'w', 'w'};
+        map[2] = new char[]{ 'w', 'w', 'w'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(1);
+        assertThat(allIslands.get(0))
+                .containsExactly(new IslandsDetector.Position(0, 2));
+    }
+
+    @Test
+    void singleIslandCenterMap() {
+        final char[][] map = new char[3][];
+        map[0] = new char[]{ 'w', 'w', 'w'};
+        map[1] = new char[]{ 'w', 'l', 'w'};
+        map[2] = new char[]{ 'w', 'w', 'w'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(1);
+        assertThat(allIslands.get(0))
+                .containsExactly(new IslandsDetector.Position(1, 1));
+    }
+
+    @Test
+    void singleIslandLeftDownMap() {
+        final char[][] map = new char[3][];
+        map[0] = new char[]{ 'w', 'w', 'w'};
+        map[1] = new char[]{ 'w', 'w', 'w'};
+        map[2] = new char[]{ 'l', 'w', 'w'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(1);
+        assertThat(allIslands.get(0))
+                .containsExactly(new IslandsDetector.Position(2, 0));
+    }
+
+    @Test
+    void singleIslandRightDownMap() {
+        final char[][] map = new char[3][];
+        map[0] = new char[]{ 'w', 'w', 'w'};
+        map[1] = new char[]{ 'w', 'w', 'w'};
+        map[2] = new char[]{ 'w', 'w', 'l'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(1);
+        assertThat(allIslands.get(0))
+                .containsExactly(new IslandsDetector.Position(2, 2));
+    }
+
+    @Test
+    void threeIslandsMap() {
+        final char[][] map = new char[3][];
+        map[0] = new char[]{ 'l', 'w', 'w'};
+        map[1] = new char[]{ 'w', 'l', 'w'};
+        map[2] = new char[]{ 'w', 'w', 'l'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(3);
+        assertThat(allIslands.get(0)).containsOnly(new IslandsDetector.Position(0, 0));
+        assertThat(allIslands.get(1)).containsOnly(new IslandsDetector.Position(1, 1));
+        assertThat(allIslands.get(2)).containsOnly(new IslandsDetector.Position(2, 2));
+    }
+
+    @Test
+    void twoIslandsMap() {
+        final char[][] map = new char[3][];
+        map[0] = new char[]{ 'l', 'l', 'w'};
+        map[1] = new char[]{ 'l', 'w', 'l'};
+        map[2] = new char[]{ 'w', 'w', 'l'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(2);
+        assertThat(allIslands.get(0)).containsOnly(
+                new IslandsDetector.Position(0, 0),
+                new IslandsDetector.Position(1, 0),
+                new IslandsDetector.Position(0, 1));
+
+        assertThat(allIslands.get(1)).containsOnly(
+                new IslandsDetector.Position(1, 2),
+                new IslandsDetector.Position(2, 2));
+    }
+
+    @Test
+    void twoIslands1Map() {
+        final char[][] map = new char[5][];
+        map[0] = new char[]{ 'l', 'l', 'l', 'l', 'l'};
+        map[1] = new char[]{ 'l', 'w', 'w', 'w', 'l'};
+        map[2] = new char[]{ 'l', 'w', 'l', 'w', 'l'};
+        map[3] = new char[]{ 'l', 'w', 'w', 'w', 'l'};
+        map[4] = new char[]{ 'l', 'l', 'l', 'l', 'l'};
+
+        final List<Collection<IslandsDetector.Position>> allIslands = islandsDetector.getAllIslands(map);
+
+        assertThat(allIslands).hasSize(2);
+        assertThat(allIslands.get(0)).containsOnly(
+                new IslandsDetector.Position(0, 0),
+                new IslandsDetector.Position(0, 1),
+                new IslandsDetector.Position(0, 2),
+                new IslandsDetector.Position(0, 3),
+                new IslandsDetector.Position(0, 4),
+
+                new IslandsDetector.Position(1, 0),
+                new IslandsDetector.Position(1, 4),
+
+                new IslandsDetector.Position(2, 0),
+                new IslandsDetector.Position(2, 4),
+
+                new IslandsDetector.Position(3, 0),
+                new IslandsDetector.Position(3, 4),
+
+                new IslandsDetector.Position(4, 0),
+                new IslandsDetector.Position(4, 1),
+                new IslandsDetector.Position(4, 2),
+                new IslandsDetector.Position(4, 3),
+                new IslandsDetector.Position(4, 4)
+                );
+
+        assertThat(allIslands.get(1)).containsOnly(
+                new IslandsDetector.Position(2, 2));
     }
 }
